640c7e19a4513aa595480334f85e75b612a18c56
Sam Lozano <sgonza20@vols.utk.edu>
2024-07-25 21:14:06 -0400

    chore: add db connection

--- go/main/main.go
+++ go/main/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	db, err := sql.Open("mysql", "mydatabase:password(127.0.0.1:3306)/mydatabase") // Open connection to MySQL DB
+
+	if err != nil { // Check if an error occured
+		panic(err) // If so, return the error
+	}
+	ping := db.Ping() // ping the database
+	if ping != nil {  // check if ping returned an error
+		fmt.Println("Error pinging database", ping) // If so, print error message
+	}
+
 }
